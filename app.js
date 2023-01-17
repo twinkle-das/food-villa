135aadc88eeb9589c8714abce30d45af090b6d7e
Twinkle Das <twinkledas086@gmail.com>
2023-01-18 01:17:10 +0530

    1. Created hello world program using HTML only. 2. Manipulating DOM using JS.

---
+++ app.js
@@ -0,0 +1,4 @@
+const divElement = document.createElement('div')
+const rootElement = document.querySelector('#root')
+divElement.innerText = 'HELLO WORLD from JS!!'
+rootElement.appendChild(divElement)
