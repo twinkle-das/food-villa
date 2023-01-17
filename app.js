7028f880dc90dc6fceadab4dc29baf39d16b8ecd
Twinkle Das <twinkledas086@gmail.com>
2023-01-18 02:15:53 +0530

    1) Injecting React. 2) used React createElement and nested element. 3) used createRoot and render().

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-const divElement = document.createElement('div')
-const rootElement = document.querySelector('#root')
-divElement.innerText = 'HELLO WORLD from JS!!'
-rootElement.appendChild(divElement)
+const heading1 = React.createElement("h1", {}, "Hello World from React!!");
+const heading2 = React.createElement(
+  "h2",
+  null,
+  "This is my first React Project."
+);
+console.log(heading2);
+const container = React.createElement("div", {id: 'main-container'}, [heading1, heading2]);
+const rootElement = ReactDOM.createRoot(document.querySelector("#root"));
+
+rootElement.render(container);
